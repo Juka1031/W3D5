adab375f3d3eac124677aed3921401f3bb96d532
julian <juka1031@gmail.com>
2021-07-02 13:58:55 -0400

    first swap

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
-# comment 
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @value
+    end
+
+    def parent=(node) #node is the new parent
+        @parent = node
+        #we want to add children(grand children) to the new parent(node)
+        @children.each |do|
+    end
 end
